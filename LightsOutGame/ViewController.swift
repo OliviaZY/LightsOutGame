dd489ecfd9eb270c40d05b3c8f12d410aa20e3a7
Yuan zhou <yuanzhou@yuans-macbook-air.local>
2018-03-20 09:15:43 -0400

    Initial Commit

---
+++ LightsOutGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LightsOutGame
+//
+//  Created by Yuan zhou on 3/20/18.
+//  Copyright © 2018 Yuan zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
